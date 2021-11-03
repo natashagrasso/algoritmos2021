44a3fa97dfb6ce02057a8a8d3eec0659395f31ee
natashagrasso <natiimetralle@gmail.com>
2021-11-03 16:26:47 -0300

    carpeta

--- ejercicio17.py
+++
@@ -1,9 +0,0 @@
-"""  Desarrollar un algoritmo que permita cargar procesos a la cola de ejecución de un procesador, 
-de los cuales se conoce id del proceso y tiempo de ejecución. Resuelva las siguientes situaciones:
-a. simular la atención de los procesos de la cola transcurriendo su tiempo –utilizando la función time.sleep (segundos) – hasta que se vacíe la cola.
-b. considerar que el quantum de tiempo asignado por el procesador a cada proceso es como 
-máximo 4.5 segundos –si el proceso no terminó su ejecución deberá volver a la cola con el 
-tiempo restante para terminar su ejecución–.
-c. cuando se realiza el cambio de proceso, porque finalizó su ejecución o porque se le agotó el 
-quantum de tiempo, se pueden ingresar nuevos procesos a la cola por el usuario.
-d. no se aplican criterios de prioridad en los procesos. """
