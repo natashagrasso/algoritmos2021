092cebe8f1eea24256bbf788c978efc212380966
natashagrasso <natiimetralle@gmail.com>
2021-11-03 16:11:33 -0300

    ejericios

--- ejercicio6.py
+++
@@ -1,9 +0,0 @@
-#Dada una secuencia de caracteres, obtener dicha secuencia invertida.
-
-def invertir_cadena(cadena):
-    if (len(cadena)==0):
-        return ''
-    else:
-        return cadena[-1]+invertir_cadena(cadena [0:-1])    
-
-print(invertir_cadena('hola mundo'))        
